cd948906bfaacbafe42ad22166381880c8f7ce05
suraj Yadav <surajyadav.sy@outlook.com>
2022-10-19 12:14:51 +0530

    Add Prime & Pallindrome File

---
+++ pallindrome.js
@@ -0,0 +1,6 @@
+var str ='aba'
+var rev =''
+for (let i=str.length-1;i>=0;i++){
+    rev+=str[i]
+}
+console.log(rev)
---
+++ prime.js
@@ -0,0 +1,9 @@
+
+var num=13;
+var count=0;
+for (let i=1;i<=num;i++){
+    if(num%i==0){
+        count++
+    }
+}
+console.log(count)
